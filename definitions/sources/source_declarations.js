b51b16e4a37625522e0ade1480df8aae6640e97e
OliverSilveti <oliverssanchez@gmail.com>
2025-12-25 11:37:33 +0100

    fixd complie error for 00-bootstrap

---
+++ definitions/sources/source_declarations.js
@@ -0,0 +1,35 @@
+declare({
+  name: "amazon_product_details_coffee_machines",
+  database: "etrendo-prd",
+  schema: "amazon_bronze"
+});
+
+declare({
+  name: "amazon_price_listing_coffee_machines",
+  database: "etrendo-prd",
+  schema: "amazon_bronze"
+});
+
+declare({
+  name: "listings",
+  database: "etrendo-prd",
+  schema: "amazon_bronze"
+});
+
+declare({
+  name: "reviews",
+  database: "etrendo-prd",
+  schema: "amazon_bronze"
+});
+
+declare({
+  name: "otto_product_listing_coffee_machines",
+  database: "etrendo-prd",
+  schema: "otto_bronze"
+});
+
+declare({
+  name: "amazon_product_listing_coffee_machines",
+  database: "etrendo-prd",
+  schema: "amazon_bronze"
+});
